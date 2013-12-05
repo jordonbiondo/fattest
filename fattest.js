6686fa5f521566aba9194e435907bc3222ff43f6
jordonbiondo <biondoj@mail.gvsu.edu>
2013-12-04 23:48:59 -0500

    header

--- fattest.js
+++ fattest.js
@@ -1,3 +1,40 @@
+/* fattest.js --- tiny little testing tool
+ * 
+ * Filename: fattest.js
+ * Description: tiny little testing tool
+ * Author: Jordon Biondo jordonbiondo@gmail.com
+ * Created: Wed Dec  4 22:33:03 2013 (-0500)
+ * Last-Updated: Wed Dec  4 23:48:16 2013 (-0500)
+ *           By: Jordon Biondo
+ *     Update #: 3
+ * URL: github.com/jordonbiondo/fattest
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* Code: */
+
+
 /**
  * Fattest, a small unit testing tool
  */
